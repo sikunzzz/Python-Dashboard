04e22df134e8099c60522c276ce9f1f599a92d23
sikunzzz <sikun.zeng95@gmail.com>
2019-10-27 13:31:35 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,129 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+import request_data as rd
+import plotly.graph_objs as go
+import pandas as pd
+from datetime import datetime
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+filenames = []
+filenames.append(os.getcwd() + "/TRdata_oil.json" )
+filenames.append(os.getcwd() + "/TRdata_gas.json" )
+#df_dict = rd.load_TRTickdata(filenames, ["CLc1", "NGc1"])
+
+final_df = rd.load_TRHistdata(["CLc1","NGc1"], "2016-11-01", "2018-10-31", os.getcwd() + "/TRHist.json")
+final_df["CLc1"] = 0.13*final_df["CLc1"]
+final_df.rename(columns={"CLc1": "13% CLc1"}, inplace=True)
+
+
+app.layout = html.Div([
+    html.H3('Dashboard'),
+    html.H4('data collected from Thomson Reuter Refinitiv'),
+
+    html.Div([
+        dcc.Graph(id="oil-gas-plot"
+         )],
+        style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}
+        ),
+
+    # html.Div(children=[html.H4(children='Gas Monthly Summary'), rd.generate_table(monthly_summary)],
+    #      style={"width": '49%', 'display': 'inline-block'})
+    html.Div([
+        dcc.Graph(id="gas-summary"
+               )],
+        style={'display': 'inline-block', 'width': '49%'}
+        ),
+
+    html.Div(dcc.DatePickerRange(
+            id = 'date-picker',
+            min_date_allowed=datetime(2016,11,1),
+            max_date_allowed=datetime(2018,10,31),
+            initial_visible_month=datetime(2017,10,31),
+            end_date=datetime(2018,10,31)
+    ), #className="six columns")
+        style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+    ]
+    )
+
+
+@app.callback(
+    (dash.dependencies.Output('oil-gas-plot', 'figure')),
+    [dash.dependencies.Input('date-picker', "start_date"),
+     dash.dependencies.Input('date-picker', "end_date")])
+def update_graph(start_date, end_date):
+
+    if start_date is None:
+        start_date=datetime(2016,11,1)
+    if end_date is None:
+        end_date=datetime(2018,10,31)
+
+    sub_df = final_df.loc[start_date:end_date]
+
+    return {
+        'data': [go.Scatter(
+            x=sub_df.index,
+            y=sub_df[i],
+            mode = 'lines+markers',
+            opacity=0.7,
+            marker={
+                'size': 1,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ) for i in sub_df.columns],
+        'layout': go.Layout(
+            xaxis={'title': 'Date'},
+            yaxis={'title': 'Price'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            #legend={'x': 0, 'y': 1},
+            hovermode='closest')
+    }
+
+
+@app.callback(
+    (dash.dependencies.Output('gas-summary', 'figure')),
+    [dash.dependencies.Input('date-picker', "start_date"),
+     dash.dependencies.Input('date-picker', "end_date")])
+def update_summary(start_date, end_date):
+
+    if start_date is None:
+        start_date=datetime(2016,11,1)
+    if end_date is None:
+        end_date=datetime(2018,10,31)
+
+    sub_df = final_df.loc[start_date:end_date]["NGc1"]
+    sub_df = sub_df.resample("1M")
+
+    summary_df = pd.DataFrame({"Mean": sub_df.mean(),
+                               "Median": sub_df.median(),
+                               "Max":  sub_df.max(),
+                               "Min":  sub_df.min()})
+    return {
+        'data': [go.Scatter(
+            x=summary_df.index,
+            y=summary_df[i],
+            mode = 'lines+markers',
+            opacity=0.7,
+            marker={
+                'size': 1,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ) for i in summary_df.columns],
+        'layout': go.Layout(
+            xaxis={'title': 'Date'},
+            yaxis={'title': 'Price'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            #legend={'x': 0, 'y': 1},
+            hovermode='closest')
+    }
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ request_data.py
@@ -0,0 +1,126 @@
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import pandas as pd
+import json
+import pandas_gbq
+import numpy as np
+import requests
+import os
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import dash_html_components as html
+
+
+personal_cred = json.loads('{"type":"service_account","project_id":"tr-data-workbench","private_key_id":"1c715a521ff59da097178dd1ab3433ec419b84af","private_key":"-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2+XxOal6ShiEH\\ngEXk7fDvJikzPQBBGF78ULXZ6dF2exLAcVsjJfRLASx21QVS7U20up4y2tWZ1bE8\\n8lloLkJstxJsTLIAx/9+Xwj6ur6kTsUxPYD5FwQGtsE7V7W1JgsiNvRBwFc2azHU\\n+sgkF31oA44kcwBbi7ltpP11dORQOJ+ldQpFq7KnUtL/POTbTfKlBlcrrX3Rkm7c\\n/9pFcR5FEQ8ApYagsdFgYHGSay8YLkOZjHNgIpW4ybAXI5OnbIxO3T1PeZDbjpEB\\nvoFieRZ2/IuiGjVNhE2WkTgz5QMwBapoYwtRDTpUlAb5eRfSRpRvKTgFAnQpLbAm\\nPi38IbFRAgMBAAECggEAIw3V8kHvn1uh/UlmWo75Px/M+duAbngoTmd6B2cDsZKv\\ni33MC4ZMcIgniBaUgAglgG/WOgDxthiZ7YayeUKPiDtkhKoG2h52xLOZSUu4lUrf\\n/Wh+inoN0+l0SJWroqpIrMPhdK1QKAVOVTK8YCm95Uz1EOeVVHSG0EoxIq0DxphX\\ngGDcDTS372SFoI8CQxFw7CTOQCe7zwgcgJlQekmwhZm8yyCVEYzo5xlo4XciLE33\\njLpdgWjjM7YVwqh94IiK2hXmt30mY3ioVt+r0iNxe4xR2wBSOXnLGRvDGb0hPBOu\\nkbvcV+MZHcEkYjOM70+ZvM6BbIH8SCcuopVOrbFSCQKBgQDh8OOXTYrh1IIlQynj\\n/4JZzuppcX5uffkMVQqX4gZOOp2d2NIme2e0S6ix059G554jPx8bbpXor4hEeKxr\\nSjSe3mk5wqRzYXptqXoo17QPZmvpVi+2RfdnGUFhhZLJbwhEHmqLXJMe7W0e5At9\\nzyIVnzK3DheTRjIPS6pUxn2LpQKBgQDPUT5iQj0IKINSEnjKj1IJAd7Vbxwwraag\\njTOuHTFHByHi5Asdy62gF+HIRUkA9tPNUCmqhocBYAw0VpPGCRAEd3ENHXKQrMax\\nSROcbsT+mm6ymgEK2B/GPtydCjY7PPMHTmrCxsPV1koq147Ain9Ru649IOQdCv98\\ne9lm3YnPPQKBgCDbwo2PhmIfYKoAoYf58cAT2n1pNwTkpyKG/5plEZuw5Jk/Hhjg\\nm41Z73elGiXaq2He3SxFIeIMHRowHosf6JUuLUlsKDRreb2XByAHdrVCpPDzSs8M\\nT9Wbk6mWHmnTDvWxIrePyAmYZ+U1LwKl7AQO+fYrQ3x0mfUlgOkBUOLNAoGAWzZh\\nfhWqiIZakKdz4ZsA+tmJ0tnZy0j7gLPfQwGxBpEKxaqTgX51W3RmcBibsALo2PIm\\nAtLBX0eE1xooVf7yvyRV5vFH0INTv/ho2nCZ13LWtVmwj7ba8/wAUE+H4LRGMLa9\\nngiZbOGlLAg+1FObBLjQRjDMbELEeV58HhfmY/kCgYEAkxcuBL0gLq06JoLtDSC6\\n7Bfp/Fj4IV6OChAYN60SFU8VnDoopV6sA+5CiZbWzu2ob+Agh/i7PQQxSs1s1tR1\\nsauVB8DUXUqAztSfKdymKXWafpUnTmwXzsd849Z75Y0Er/HwLsgB30GDhQ+BVnKJ\\n4Ih5xzWVb6ofozTA8IdHh2E=\\n-----END PRIVATE KEY-----\\n","client_email":"s4s8bso2qk5cvv8e5ts8ajhmsq8b0e@tr-data-workbench.iam.gserviceaccount.com","client_id":"113639508953850221246","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/s4s8bso2qk5cvv8e5ts8ajhmsq8b0e%40tr-data-workbench.iam.gserviceaccount.com"}') # your personal key for Tick History on BigQuery
+proj = u'tr-data-workbench'
+cred = service_account.Credentials.from_service_account_info(personal_cred)
+
+
+def load_TRTickdata(filenames, ticker_list):
+
+    dataframe_dict = {}
+
+    for i in range(len(filenames)):
+
+        if os.path.exists(filenames[i]):
+             dataframe_dict[ticker_list[i]] = pd.read_json(filenames[i])
+
+        else:
+            query = """ SELECT
+                        RIC, Date_Time, Bid_Price, Bid_Size, Ask_Price, Ask_Size, Price, Volume, Type
+                        FROM
+                        `tr-data-workbench.Futures.L1_Q4_2018`
+                        WHERE
+                        Type in ("Trade", "Quote")
+                        AND
+                        Date_Time between '2018-10-01 9:00:00' and '2018-10-1 16:00:00'
+                        AND
+                        RIC=""" + "'" + ticker_list[i] + "'" +" ORDER BY Date_Time"
+
+            df = pandas_gbq.read_gbq(query, project_id=proj, credentials=cred, dialect='standard')
+            df['t'] = pd.to_datetime(df.Date_Time)
+            df.set_index('t', drop=True, inplace=True)
+            df = df.tz_localize(None)
+
+            tickjString = df.to_json()
+            with open(filenames[i], "+w") as f:
+                f.write(tickjString)
+
+                dataframe_dict[ticker_list[i]] = df
+
+    return dataframe_dict
+
+
+def get_data_request(ric, start_date, end_date, fields="BID"):
+
+    RESOURCE_ENDPOINT = "https://dsa-stg-edp-api.fr-nonprod.aws.thomsonreuters.com/data/historical-pricing/beta1/views/summaries/"+ ric
+    access_token = 'T1IcOXrbT89HlDos9q9uG3YlBxK4nPY31HQLTllT'
+    requestData = {
+        "interval": "P1D",
+        "start": start_date,
+        "end": end_date,
+        "fields": fields #BID,ASK,OPEN_PRC,HIGH_1,LOW_1,TRDPRC_1,NUM_MOVES,TRNOVR_UNS
+    };
+
+    dResp = requests.get(RESOURCE_ENDPOINT, headers={"X-api-key": access_token}, params=requestData)
+
+    if dResp.status_code!=200:
+        print("Unable to get data. Code %s, Message: %s" %(dResp.status_code, dResp.text))
+    else:
+        print("Data access successful " + "for ", ric)
+        jResp = json.loads(dResp.text)
+        return jResp
+
+
+def load_TRHistdata(tickerList, startDate, endDate, fn):
+
+    if os.path.exists(fn):
+        final_df = pd.read_json(fn)
+        return final_df
+    else:
+        df_list = []
+        for ticker in tickerList:
+            jResp = get_data_request(ticker, startDate, endDate)
+
+            if jResp is not None:
+                try:
+                    data = jResp[0]['data']
+                    headers = jResp[0]['headers']
+                    names = [headers[x]['name'] for x in range(len(headers))]
+                    close_price = pd.DataFrame(data, columns=names )
+                    close_price.columns = ['DATE', ticker.replace("=", "")]
+                    close_price.set_index("DATE", inplace=True)
+                    df_list.append(close_price)
+                except:
+                    next
+
+        final_df = pd.concat(df_list, axis=1, join="outer", sort=False)
+        final_df.sort_index(inplace=True)
+
+        with open(fn, "+w") as f:
+            f.write(final_df.to_json())
+
+        return final_df
+
+
+def double_axis_plotter(df, colnames_1, colnames_2):
+
+    fig, ax1 = plt.subplots(figsize=(10,7))
+
+    ax1.plot(df[colnames_1])
+    ax1.set_ylabel(colnames_1)
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
+
+    ax2 = ax1.twinx()
+    ax2.plot(df[colnames_2], 'r-')
+    ax2.set_ylabel(colnames_2, color="r")
+
+    fig.autofmt_xdate()
+
+
+def generate_table(dataframe, max_rows=20):
+
+    return html.Table(
+        [html.Tr([html.Th(col) for col in dataframe.columns]
+         for i in range(min(len(dataframe), max_rows)))]
+    )
